8ab354ef91010a1ecfb86592e38f65186e8a719b
Roman <rohoehn123@gmail.com>
2025-03-10 15:27:44 +0100

    Create quickSort.hs

---
+++ quickSort.hs
@@ -0,0 +1,16 @@
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) = (quicksort smaller) ++ [x] ++ (quicksort greaterOrEqual)
+  where
+    smaller = filter (<x) xs
+    greaterOrEqual = filter (>=x) xs
+  
+  
+unord :: [Int]
+unord = [9,1,5,3,6]
+
+sorted :: [Int]
+sorted = quicksort unord
+
+main :: IO ()
+main = putStrLn (show sorted)
