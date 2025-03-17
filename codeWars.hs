78319ab3ed305bf597f9c6c65940290fe7499779
rohoehn <rohoehn123@gmail.com>
2025-03-17 12:51:24 +0100

    new kata done

--- codeWars.hs
+++ codeWars.hs
@@ -1,3 +1,6 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Move guards forward" #-}
+
 import Data.Char (isAscii, isLetter, ord, toLower)
 import Data.List (nub)
 import Data.Set (fromList)
@@ -31,3 +34,10 @@ nbYear :: Int -> Double -> Int -> Int -> Int
 nbYear p0 percent aug p = length $ takeWhile (< p) popSizes
   where
     popSizes = popGrowth p0 percent aug
+
+findEvenIndex :: [Int] -> Int
+findEvenIndex arr = if null res then -1 else fst $ head res
+  where
+    res = [(i, x) | (i, x) <- zip [0 ..] arr, sumFirst i == sumTail i]
+    sumFirst i = sum $ take (i + 1) arr
+    sumTail i = sum $ drop i arr
