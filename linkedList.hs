8847ddb962d19e49667a2b422d9e4c5a0a3b4652
rohoehn <rohoehn123@gmail.com>
2025-03-25 17:36:31 +0100

    ...

--- linkedList.hs
+++ linkedList.hs
@@ -1,6 +1,6 @@
 module LinkedList where
 
-data LinkedList a = Empty | Node (a, LinkedList a)
+data LinkedList a = Empty | Node (a, LinkedList a) deriving (Eq)
 
 instance (Show a) => Show (LinkedList a) where
   show Empty = "Empty"
@@ -40,3 +40,37 @@ sum' = reduce' (+) 0
 
 prod' :: (Num a) => LinkedList a -> a
 prod' = reduce' (*) 1
+
+indexOf :: (Eq a) => LinkedList a -> a -> Maybe Int
+indexOf xs elem = f xs elem 0
+  where
+    f Empty _ _ = Nothing
+    f (Node (v, next)) elem currentIdx
+      | v == elem = Just currentIdx
+      | otherwise = f next elem (currentIdx + 1)
+
+contains' :: (Eq a) => LinkedList a -> a -> Bool
+contains' xs elem = case indexOf xs elem of
+  Just _ -> True
+  Nothing -> False
+
+concat' :: LinkedList a -> LinkedList a -> LinkedList a
+concat' Empty ys = ys
+concat' (Node (v, next)) ys = Node (v, concat' next ys)
+
+take' :: Int -> LinkedList a -> LinkedList a
+take' _ Empty = Empty
+take' n _ | n <= 0 = Empty
+take' n (Node (v, next)) = Node (v, take' (n - 1) next)
+
+
+
+-- tl = Empty
+
+-- t1 = prepend 1 tl
+
+-- t2 = prepend 2 t1
+
+-- t3 = prepend 3 t2
+
+-- t4 = prepend 4 t3
